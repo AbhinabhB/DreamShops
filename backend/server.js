0637b52698e01f1903b1a6fc20631096bd19b14f
AbhinabhB <155467762+AbhinabhB@users.noreply.github.com>
2025-10-28 15:51:46 +0530

    fix all catch path

--- backend/server.js
+++ backend/server.js
@@ -30,17 +30,17 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	const __filename = fileURLToPath(import.meta.url);
-	const __dirname = path.dirname(__filename);
+// if (process.env.NODE_ENV === "production") {
+// 	const __filename = fileURLToPath(import.meta.url);
+// 	const __dirname = path.dirname(__filename);
 
-	// Serve frontend
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// 	// Serve frontend
+// 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-	app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-	});
-}
+// 	app.get("/*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+// 	});
+// }
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
